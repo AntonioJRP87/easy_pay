dd5034aae0f88f3f16e8cccd3979440daf760dba
Antonio Jesus Rosa Prieto <antonio.rosaprieto@plexus.es>
2025-08-06 17:16:35 +0200

    firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
+import 'package:easy_pay/firebase_options.dart';
 import 'package:easy_pay/presentation/l10n/gen_l10n/app_localizations.dart';
 import 'package:easy_pay/presentation/themes/app_theme.dart';
 import 'package:easy_pay/routes.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
